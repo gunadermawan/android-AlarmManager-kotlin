31cd0c32e91158ca8d29f465feead3607efe2989
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-06 19:55:25 +0700

    add companion object

--- app/src/main/java/com/gunder/myalarmmanager/AlarmReceiver.kt
+++ app/src/main/java/com/gunder/myalarmmanager/AlarmReceiver.kt
@@ -5,9 +5,18 @@ import android.content.Context
 import android.content.Intent
 
 class AlarmReceiver : BroadcastReceiver() {
+    companion object{
+        const val TYPE_ONE_TIME = "OneTimeAlarm"
+        const val TYPE_REPEATING = "RepeatingAlarm"
+        const val EXTRA_MESSAGE = "message"
+        const val EXTRA_TYPE = "type"
+//    siapkan dua id untuk 2 macam alarm, one time dan repeating
+        private const val ID_ONETIME = 100
+        private const val ID_REPEATING = 101
+    }
+
 
     override fun onReceive(context: Context, intent: Intent) {
-        // This method is called when the BroadcastReceiver is receiving an Intent broadcast.
-        TODO("AlarmReceiver.onReceive() is not implemented")
+
     }
 }
