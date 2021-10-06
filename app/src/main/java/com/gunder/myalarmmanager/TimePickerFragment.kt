3dba5990337c83ce56a977d1dd97d767bb389e32
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-06 21:26:23 +0700

    membuat kosw untuk TimePicker

--- app/src/main/java/com/gunder/myalarmmanager/TimePickerFragment.kt
+++ app/src/main/java/com/gunder/myalarmmanager/TimePickerFragment.kt
@@ -1,59 +1,41 @@
 package com.gunder.myalarmmanager
 
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.content.Context
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class TimePickerFragment : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+    private var mListener: DialogTimeListener? = null
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TimePickerFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TimePickerFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        mListener = context as DialogTimeListener?
+    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    override fun onDetach() {
+        super.onDetach()
+        if (mListener != null) {
+            mListener = null
         }
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_picker, container, false)
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val calendar = Calendar.getInstance()
+        val hour = calendar.get(Calendar.HOUR_OF_DAY)
+        val minute = calendar.get(Calendar.MINUTE)
+        val formatHour24 = true
+        return super.onCreateDialog(savedInstanceState)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TimePickerFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TimePickerFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+        mListener?.onDialogTimeSet(tag, hourOfDay, minute)
+    }
+
+    interface DialogTimeListener {
+        fun onDialogTimeSet(tag: String?, hourOfDay: Int, minute: Int)
     }
 }
