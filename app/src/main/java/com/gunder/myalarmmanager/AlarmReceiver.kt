b5ef5ccba1bbf7e84be88d2d8a41950040ab11a8
Guna Dermawan <53375007+gunadermawan@users.noreply.github.com>
2021-10-06 20:03:30 +0700

    melengkapi kode onReceive dan membuat fun showToast di AlarmReceiver

--- app/src/main/java/com/gunder/myalarmmanager/AlarmReceiver.kt
+++ app/src/main/java/com/gunder/myalarmmanager/AlarmReceiver.kt
@@ -3,6 +3,7 @@ package com.gunder.myalarmmanager
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.widget.Toast
 
 class AlarmReceiver : BroadcastReceiver() {
     companion object{
@@ -17,6 +18,17 @@ class AlarmReceiver : BroadcastReceiver() {
 
 
     override fun onReceive(context: Context, intent: Intent) {
+        val type = intent.getStringExtra(EXTRA_TYPE)
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
 
+        val title = if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) TYPE_ONE_TIME else TYPE_REPEATING
+        val notifId = if (type.equals(TYPE_ONE_TIME, ignoreCase = true)) ID_ONETIME else ID_REPEATING
+
+        showToast(context, title, message)
+
+    }
+
+    private fun showToast(context: Context, title: String, message: String?) {
+        Toast.makeText(context, "$title : $message", Toast.LENGTH_SHORT).show()
     }
 }
